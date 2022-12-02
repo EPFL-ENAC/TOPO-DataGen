96a303f4c36b3d5a1a910f9f274b9020662428ae
Régis Longchamp <97044425+regislon@users.noreply.github.com>
2022-12-02 14:31:50 +0100

    :bug: fix for ducumentatîon generation

--- scripts/start_generate.py
+++ scripts/start_generate.py
@@ -18,12 +18,12 @@ import re
 from config import settings
 
 
-SCRIPT_DIR = os.path.dirname(os.path.realpath('__file__'))
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 print('SCRIPT_DIR',SCRIPT_DIR)
 
 
 USER_HOME = os.path.expanduser("~")
-PROJ_DIR = os.path.abspath(os.path.join('__file__', '../..'))
+PROJ_DIR = os.path.abspath(os.path.join(__file__, '../..'))
 sys.path.insert(0, PROJ_DIR)
 from reframeTransform import ReframeTransform
 
